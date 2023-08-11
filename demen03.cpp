01f7b759aa2e0a70895fdc531852aef4c8cf8198
tkkiethotuan <97288041+tkkiethotuan@users.noreply.github.com>
2023-08-11 21:57:05 +0700

    Update demen03.cpp

--- demen03.cpp
+++ demen03.cpp
@@ -19,14 +19,14 @@ void backtrack(ll pos, ll cow_villa_1, ll cow_villa_2, ll homeless_cow) {
     for (long long i = 1; i <= 3; i ++) {
         if (i == 1) cow_villa_1 += cow_list[pos];
         if (i == 2) cow_villa_2 += cow_list[pos];
-        if (i == 3) homeless_cow += cow_list[pos];
+        if (i == 3) homeless_cow ++;
 
         if (pos == n) solve(cow_villa_1, cow_villa_2, homeless_cow);
         else backtrack(pos + 1, cow_villa_1, cow_villa_2, homeless_cow);
 
         if (i == 1) cow_villa_1 -= cow_list[pos];
         if (i == 2) cow_villa_2 -= cow_list[pos];
-        if (i == 3) homeless_cow -= cow_list[pos];
+        if (i == 3) homeless_cow --;
     }
 }
 
@@ -38,4 +38,5 @@ int main() {
     sort(cow_list + 1, cow_list + 1 + n);
     min_homeless_cow = cow_list[n];
     backtrack(1, 0, 0, 0);
+    cout << min_homeless_cow << '\n';
 }
